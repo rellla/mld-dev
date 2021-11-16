4181b53113b79733ffedace8946d3e0ded39a9ff
Claus Muus <claus@minidvblinux.de>
2021-11-16 22:41:51 +0000

    add script that build all images and packages

--- buildall.sh
+++ buildall.sh
@@ -12,7 +12,7 @@ cd ${0%/*}
 
   for machine in x86 x86-qemu rpi4; do
     kas build mld6-$machine.yml
-    kas shell mld6-$machine.yml -c "bitbake packagegroup-vdr"
+    kas shell mld6-$machine.yml -c "bitbake packagegroup-all"
     kas shell mld6-$machine.yml -c "bitbake package-index"
   done
   

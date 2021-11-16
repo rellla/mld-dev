b95f825443f72338e2db123fbb2c82853aaf63d8
Claus Muus <claus@minidvblinux.de>
2021-11-16 22:34:56 +0000

    add script that build all images and packages

--- buildall.sh
+++ buildall.sh
@@ -3,11 +3,18 @@
 
 cd ${0%/*}
 
-git pull
-(cd meta-mld; git pull)
+{
+  echo '----'
+  date
 
-for machine in x86 x86-qemu rpi4; do
-  kas build mld6-$machine.yml
-  kas shell mld6-$machine.yml -c "bitbake packagegroup-vdr"
-  kas shell mld6-$machine.yml -c "bitbake package-index"
-done
+  git pull
+  (cd meta-mld; git pull)
+
+  for machine in x86 x86-qemu rpi4; do
+    kas build mld6-$machine.yml
+    kas shell mld6-$machine.yml -c "bitbake packagegroup-vdr"
+    kas shell mld6-$machine.yml -c "bitbake package-index"
+  done
+  
+  date
+} > buildall.log

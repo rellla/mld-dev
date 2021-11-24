1683cfd213784ee6fd2ad45c2c3297ffda76f3ce
Claus Muus <claus@minidvblinux.de>
2021-11-24 10:01:32 +0000

    set USER enviroment variable if missing

--- buildall.sh
+++ buildall.sh
@@ -8,6 +8,8 @@ cd ${0%/*}
 git pull
 (cd meta-mld; git pull)
 
+export ${USER:-$(whoami)}
+
 for machine in x86 x86-qemu rpi4; do
   /usr/local/bin/kas build mld6-$machine.yml
   /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake packagegroup-all"

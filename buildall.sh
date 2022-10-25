3e24797a4c2734e0baec9666b4b0ebc333a8123c
Claus Muus <claus@minidvblinux.de>
2022-10-25 20:36:50 +0000

    activate build of all pakages

--- buildall.sh
+++ buildall.sh
@@ -12,6 +12,6 @@ export USER=${USER:-$(whoami)}
 
 for machine in x86 x86-qemu rpi2 rpi3 rpi4; do
   /usr/local/bin/kas build mld6-$machine.yml
-  /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake packagegroup-all"
+  /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake --runall build packagegroup-all"
   /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake package-index"
 done

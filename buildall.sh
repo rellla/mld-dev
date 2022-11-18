a6e1c1c10dd211107de8223d5a4b5592127c74cf
Roland <roland@diefehrs.de>
2022-11-18 19:19:31 +0000

    add rock-pi-4

--- buildall.sh
+++ buildall.sh
@@ -10,7 +10,7 @@ git pull
 
 export USER=${USER:-$(whoami)}
 
-for machine in x86 x86-qemu rpi2 rpi3 rpi4; do
+for machine in x86 x86-qemu rpi2 rpi3 rpi4 rock-pi-4; do
   /usr/local/bin/kas build mld6-$machine.yml
   /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake --runall build packagegroup-all"
   /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake package-index"

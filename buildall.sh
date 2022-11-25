cb60c8fff33c2cfe8be2b5bd6e291ef83f1389f5
Roland <roland@diefehrs.de>
2022-11-25 18:45:29 +0000

    build all packagegroups single

--- buildall.sh
+++ buildall.sh
@@ -12,7 +12,11 @@ export USER=${USER:-$(whoami)}
 
 for machine in x86 x86-qemu rpi2 rpi3 rpi4 rock-pi-4; do
   /usr/local/bin/kas build mld6-$machine.yml
-  /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake --runall build packagegroup-all"
+  /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake --runall build packagegroup-vdr"
+  /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake --runall build packagegroup-extra"
+  /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake --runall build packagegroup-oscam"
+  /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake --runall build packagegroup-tools"
+  /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake --runall build packagegroup-alsa"
   /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake package-index"
 done
 

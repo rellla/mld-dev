a2bea2877ee4b6df0e0faeb68da41cec55642605
Claus Muus <claus@minidvblinux.de>
2022-12-18 23:36:42 +0100

    extend build all

--- buildall.sh
+++ buildall.sh
@@ -12,13 +12,14 @@ export USER=${USER:-$(whoami)}
 
 for machine in x86 x86-qemu rpi2 rpi3 rpi4 rock-pi-4; do
   /usr/local/bin/kas build mld6-$machine.yml
-  /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake --runall build packagegroup-vdr"
+  /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake --runall build packagegroup-addons"
+  /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake --runall build packagegroup-alsa"
   /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake --runall build packagegroup-extra"
+  /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake --runall build packagegroup-gstreamer"
+  /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake --runall build packagegroup-kernel-modules"
   /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake --runall build packagegroup-oscam"
   /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake --runall build packagegroup-tools"
-  /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake --runall build packagegroup-alsa"
-  /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake --runall build packagegroup-gstreamer"
-  /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake --runall build packagegroup-addons"
+  /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake --runall build packagegroup-vdr"
   /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake package-index"
 done
 

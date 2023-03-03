58506a8d7a268254dfba6848a984d59a6ebd7852
Roland <roland@diefehrs.de>
2023-03-03 22:54:26 +0100

    add packagegroup-webbrowser

--- buildall.sh
+++ buildall.sh
@@ -23,6 +23,7 @@ for machine in x86 x86-qemu rpi2 rpi3 rpi4 rock-pi-4; do
   /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake --runall build packagegroup-tools"
   /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake --runall build packagegroup-vdr"
   /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake --runall build packagegroup-debug"
+  /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake --runall build packagegroup-webbrowser"
   /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake package-index"
 done
 

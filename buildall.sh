ca94fbdc04a97d96c6bb2896b04ab905ce0c97a7
Claus Muus <claus@minidvblinux.de>
2023-04-02 16:14:38 +0000

    add option -k to bitbake build and use packagegroup-all

--- buildall.sh
+++ buildall.sh
@@ -12,25 +12,18 @@ export USER=${USER:-$(whoami)}
 
 for machine in x86 x86-qemu rpi2 rpi3 rpi4 rock-pi-4 tinker-board tinker-board-s; do
   /usr/local/bin/kas build mld6-$machine.yml
-  /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake --runall build packagegroup-addons"
-  /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake --runall build packagegroup-alsa"
-  /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake --runall build packagegroup-extra"
-  /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake --runall build packagegroup-gstreamer"
-  /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake --runall build packagegroup-tools"
-  /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake --runall build packagegroup-vdr"
-  /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake --runall build packagegroup-debug"
+  /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake -k --runall build packagegroup-all"
   /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake package-index"
 done
 
 for machine in bpi cubietruck; do
   /usr/local/bin/kas build mld6-$machine.yml
-  /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake --runall build mld-image-boot"
-  /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake --runall build packagegroup-extra"
+  /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake -k --runall build mld-image-boot"
+  /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake -k --runall build packagegroup-extra"
   /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake package-index"
 done
 
 for machine in x86 x86-qemu rpi2 rpi3 rpi4 rock-pi-4 tinker-board tinker-board-s; do
-  /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake --runall build packagegroup-kernel-modules"
-  /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake --runall build packagegroup-webbrowser"
+  /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake -k --runall build packagegroup-webbrowser"
   /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake package-index"
 done

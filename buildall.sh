dc6313e1320b3b45deb25f1142cfbbe35b457282
Roland Fehr <roland@diefehrs.de>
2023-04-13 15:27:29 +0000

    add packagegroup-core-nfs

--- buildall.sh
+++ buildall.sh
@@ -13,6 +13,7 @@ export USER=${USER:-$(whoami)}
 for machine in x86 x86-qemu rpi2 rpi3 rpi4 rock-pi-4 tinker-board tinker-board-s; do
   /usr/local/bin/kas build mld6-$machine.yml
   /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake -k --runall build packagegroup-all"
+  /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake -k --runall build packagegroup-core-nfs"
   /usr/local/bin/kas shell mld6-$machine.yml -c "bitbake package-index"
 done
 

8485a1a02440ac636f789df226bb9665b651c6a2
Claus Muus <claus@minidvblinux.de>
2024-08-31 10:40:59 +0200

    cleanup buildall

--- buildall.sh
+++ buildall.sh
@@ -13,8 +13,6 @@ export USER=${USER:-$(whoami)}
 for machine in x86 x86-qemu rpi2 rpi3 rpi4 rock-pi-4 tinker-board tinker-board-s odroidn2l-hardkernel odroidn2plus-hardkernel; do
   kas build mld6-$machine.yml
   kas shell mld6-$machine.yml -c "bitbake -k --runall build packagegroup-all"
-  kas shell mld6-$machine.yml -c "bitbake -k --runall build packagegroup-core-nfs"
-  kas shell mld6-$machine.yml -c "bitbake -k --runall build docker-moby"
   kas shell mld6-$machine.yml -c "bitbake package-index"
 done
 

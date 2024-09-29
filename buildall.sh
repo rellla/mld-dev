0f2a97755219af1c2b852becad6565447048236c
Roland Fehr <roland@diefehrs.de>
2024-09-29 15:53:09 +0200

    add rpi4-64 and rpi5

--- buildall.sh
+++ buildall.sh
@@ -10,38 +10,9 @@ git pull
 
 export USER=${USER:-$(whoami)}
 
-#for machine in x86 x86-qemu rpi2 rpi3 rpi4 rpi4-64 rpi5 rock-pi-4 tinker-board tinker-board-s odroidn2l-hardkernel odroidn2plus-hardkernel; do
-#  kas build mld6-$machine.yml
-#  kas shell mld6-$machine.yml -c "bitbake -k --runall build packagegroup-all"
-#  kas shell mld6-$machine.yml -c "bitbake package-index"
-#done
-
-#for machine in odroidn2l-hardkernel odroidn2plus-hardkernel; do
-#  kas build mld6-$machine.yml
-#  kas shell mld6-$machine.yml -c "bitbake -k --runall build packagegroup-all"
-#  kas shell mld6-$machine.yml -c "bitbake package-index"
-#done
-
-#for machine in bpi cubietruck; do
-#  kas build mld6-$machine.yml
-#  kas shell mld6-$machine.yml -c "bitbake -k --runall build oscam"
-#  kas shell mld6-$machine.yml -c "bitbake -k --runall build mld-image-boot"
-#  kas shell mld6-$machine.yml -c "bitbake package-index"
-#done
-
-#for machine in x86 x86-qemu rpi2 rpi3 rpi4 rock-pi-4 tinker-board tinker-board-s; do
-#  kas shell mld6-$machine.yml -c "bitbake -k --runall build packagegroup-webbrowser"
-#  kas shell mld6-$machine.yml -c "bitbake package-index"
-#done
-
-for machine in rpi4-64; do
+for machine in x86 x86-qemu rpi2 rpi3 rpi4 rpi4-64 rpi5 rock-pi-4 tinker-board tinker-board-s odroidn2l-hardkernel odroidn2plus-hardkernel; do
   kas build mld6-$machine.yml
   kas shell mld6-$machine.yml -c "bitbake -k --runall build packagegroup-all"
   kas shell mld6-$machine.yml -c "bitbake package-index"
 done
 
-#for machine in rpi5; do
-#  kas build mld6-$machine.yml
-#  kas shell mld6-$machine.yml -c "bitbake -k --runall build packagegroup-all"
-#  kas shell mld6-$machine.yml -c "bitbake package-index"
-#done

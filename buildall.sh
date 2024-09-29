396c215b00be77f7d187dce83d46bea352cc1ee9
Claus Muus <claus@minidvblinux.de>
2024-09-29 20:48:39 +0200

    remove RPI-4 32Bit

--- buildall.sh
+++ buildall.sh
@@ -10,7 +10,7 @@ git pull
 
 export USER=${USER:-$(whoami)}
 
-for machine in x86 x86-qemu rpi2 rpi3 rpi4 rpi4-64 rpi5 rock-pi-4 tinker-board tinker-board-s odroidn2l-hardkernel odroidn2plus-hardkernel; do
+for machine in x86 x86-qemu rpi2 rpi3 rpi4 rpi5 rock-pi-4 tinker-board tinker-board-s odroidn2l-hardkernel odroidn2plus-hardkernel; do
   kas build mld6-$machine.yml
   kas shell mld6-$machine.yml -c "bitbake -k --runall build packagegroup-all"
   kas shell mld6-$machine.yml -c "bitbake package-index"

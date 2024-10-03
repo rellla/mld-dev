ee4701674db30b0b9aa49689dea6c3976283db71
Claus Muus <claus@minidvblinux.de>
2024-10-03 09:28:23 +0200

    extend buildall logs

--- buildall.sh
+++ buildall.sh
@@ -6,13 +6,16 @@ umask 0002
 cd ${0%/*}
 
 git pull
-(cd meta-mld; git pull)
+(cd meta-mld && git pull)
 
 export USER=${USER:-$(whoami)}
 
 for machine in x86 x86-qemu rpi2 rpi3 rpi4 rpi5 rock-pi-4 tinker-board tinker-board-s odroidn2l-hardkernel odroidn2plus-hardkernel; do
+  echo "------------------------------------"
+  echo "Build $machine image"
   kas build mld6-$machine.yml
+  echo "------------------------------------"
+  echo "Build $machine packages"
   kas shell mld6-$machine.yml -c "bitbake -k --runall build packagegroup-all"
   kas shell mld6-$machine.yml -c "bitbake package-index"
 done
-

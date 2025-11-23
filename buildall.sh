9cfbbc0ac04489a83746c2eb92b6556bb3208be0
Claus Muus <claus@minidvblinux.de>
2025-11-23 18:44:16 +0100

    change buildall build order

--- buildall.sh
+++ buildall.sh
@@ -11,14 +11,16 @@ git pull
 export USER=${USER:-$(whoami)}
 
 for machine in x86 x86-qemu rpi2 rpi3 rpi4 rpi5 rock-pi-4 tinker-board tinker-board-s odroidn2l-hardkernel odroidn2plus-hardkernel; do
+  #echo "------------------------------------"
+  #echo "Clear $machine recipes cache"
+  #kas shell mld6-$machine.yml -c "bitbake -S parse ''"
   echo "------------------------------------"
-  echo "Clear $machine recipes cache"
-  kas shell mld6-$machine.yml -c "bitbake -S parse ''"
+  echo "Build $machine packages"
+  kas shell mld6-$machine.yml -c "bitbake -k --runall build packagegroup-all"
   echo "------------------------------------"
   echo "Build $machine image"
   kas build mld6-$machine.yml
   echo "------------------------------------"
-  echo "Build $machine packages"
-  kas shell mld6-$machine.yml -c "bitbake -k --runall build packagegroup-all"
+  echo "Build $machine index"
   kas shell mld6-$machine.yml -c "bitbake package-index"
 done

0fba34a401ae65e0605fe1d4d865e0092b23fc73
Andreas Baierl <ichgeh@imkreisrum.de>
2026-04-07 09:24:26 +0200

    device: add Radxa Rock Pi 4B Plus

--- buildall.sh
+++ buildall.sh
@@ -10,7 +10,7 @@ git pull
 
 export USER=${USER:-$(whoami)}
 
-for machine in intel x86 x86-qemu rpi2 rpi3 rpi4 rpi5 rock-pi-4 odroidn2l-hardkernel odroidn2plus-hardkernel; do
+for machine in intel x86 x86-qemu rpi2 rpi3 rpi4 rpi5 rock-pi-4 rock-pi-4b-plus odroidn2l-hardkernel odroidn2plus-hardkernel; do
   #echo "------------------------------------"
   #echo "Clear $machine recipes cache"
   #kas shell mld6-$machine.yml -c "bitbake -S parse ''"
